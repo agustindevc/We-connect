6271765264fcb0299af54da2b1aaf2d0c94aa876
Agustin <agustind1212@gmail.com>
2025-04-04 19:24:14 +0200

    correccion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Profile from "./pages/Profile/Profile";
@@ -8,14 +8,14 @@ import "./styles/theme.css"; // Importa los estilos globales
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter basename="/We-connect">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/product" element={<Product />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { ghPages } from 'vite-plugin-gh-pages'
 
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: '/We-connect/'  //
+  base: '/We-connect/' 
 })
