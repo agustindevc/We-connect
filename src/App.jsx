751cae5bb0eb994d4cd83ad866a469a92326fa20
ErnNeyra <168720350+ErnNeyra@users.noreply.github.com>
2025-04-07 13:20:11 +0200

    Ajuste de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Profile from "./pages/Profile/Profile";
 import Product from "./pages/Product/Product";
-import "./styles/theme.css"; // Importa los estilos globales
+import "./styles/theme.css";
 
 const App = () => {
   return (
     <BrowserRouter basename="/We-connect">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/product" element={<Product />} />
-      </Routes>
+      <div className="main-container">
+        {/* Header común (opcional) */}
+        {/* <header>Navbar</header> */}
+
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/product" element={<Product />} />
+          </Routes>
+        </main>
+
+        {/* Footer común (opcional) */}
+        {/* <footer>Footer</footer> */}
+      </div>
     </BrowserRouter>
   );
 };
