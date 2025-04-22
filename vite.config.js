da6fe4192e51e55f1eec8b71ca9bd29f5f896c24
Ivan23L <ivanlzrz23@gmail.com>
2025-04-22 18:18:56 +0200

    hhhhhhhhhh

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './We-connect',
+  base: '/We-connect/',
   plugins: [react()]
 })
